af8dc9516cdc239902e656bfba2d4645e4bdfaa0
Martez Reed <martez.reed@gmail.com>
2024-03-14 15:17:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "foo" {
   content  = "foo!"
   filename = "${path.module}/foo.bar"
 }
+
+resource "local_file" "foo2" {
+  content  = "foo!"
+  filename = "${path.module}/foo2.bar"
+}
